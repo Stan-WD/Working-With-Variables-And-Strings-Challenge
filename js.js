c049067d0d1f36c28cea387ac1cd5b3d9b7ff937
Stan <ksstan@gmail.com>
2022-05-30 16:57:40 +0800

    Initial and Final Commit

---
+++ js.js
@@ -0,0 +1,9 @@
+alert("Hello and Welcome to my Page!");
+
+var userName = prompt("Can I have your name please?");
+var userFavouriteColour = prompt("May I know your favourite colour?");
+
+document.write("Hello " + userName + ", Your Favourite Colour is " + userFavouriteColour + ".");
+
+console.log("Program Completed!");
+
